09a34c526cd4e60e63fdd523d1e92608ac5c97ad
ZhangQixiang123 <e1264831@u.nus.edu>
2026-02-05 15:09:24 +0800

    fix download

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
       <img src="/photo.jpg" alt="Profile" className="profile-image" />
       <h1 className="name">Zhang Qixiang</h1>
       <p className="tagline">Undergraduate Student in NUS School of Computing</p>
-      <a href="/cv.pdf" download className="download-cv">
+      <a href="/CV.pdf" download className="download-cv">
         Download CV
       </a>
     </header>
